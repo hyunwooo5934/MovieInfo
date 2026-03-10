85b6871739414c5fad3287b7d771f0d378722cf8
김현우 <>
2026-03-10 13:31:05 +0900

    Initial commit

---
+++ app/src/main/java/com/example/movieinfoex/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.example.movieinfo
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.movieinfoex.ui.theme.MovieInfoExTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MovieInfoExTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        color = Color.Red,
+        modifier = modifier
+    )
+}
+
+@Preview
+@Composable
+fun GreetingPreview() {
+    MovieInfoExTheme {
+        Greeting("Android")
+    }
+}
